71ebacc9e57ace30e2766563bef814a479a620b4
Atnatewoss <icodeyt21@gmail.com>
2026-03-15 14:40:18 +0300

    feat: add centralized configuration system using YAML

---
+++ server/configs/settings.py
@@ -0,0 +1,25 @@
+import os
+import yaml
+
+# Project Root (server/ folder)
+CONFIG_DIR = os.path.dirname(__file__)
+BASE_DIR = os.path.abspath(os.path.join(CONFIG_DIR, ".."))
+
+YAML_PATH = os.path.join(CONFIG_DIR, "training.yaml")
+
+with open(YAML_PATH, 'r') as f:
+    config = yaml.safe_load(f)
+
+# Data paths
+DATA_PATH = os.path.abspath(os.path.join(BASE_DIR, config['data']['dataset_path']))
+MODEL_SAVE_PATH = os.path.abspath(os.path.join(BASE_DIR, config['data']['model_save_path']))
+LOG_FILE_PATH = os.path.abspath(os.path.join(BASE_DIR, config['data']['log_path']))
+
+# Hyperparameters
+BATCH_SIZE = config['training']['batch_size']
+EPOCHS = config['training']['epochs']
+LEARNING_RATE = config['training']['learning_rate']
+TRAIN_SPLIT = config['training']['train_split']
+
+# Image details
+IMG_SIZE = tuple(config['preprocessing']['img_size'])
