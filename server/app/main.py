b6b8c4a51915ab177bb8a942d02d6d442c5a231e
Atnatewoss <icodeyt21@gmail.com>
2026-03-19 12:26:40 +0300

    chore(server): minor updates to main application entrypoint

--- server/app/main.py
+++ server/app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routes import router
 
 app = FastAPI(title="TrueFrame AI - Deepfake Detection API")
+
+# Add CORS Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # In production, I will replace it with actual frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Include modular routes
 app.include_router(router)
